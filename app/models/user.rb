2ae791bf46e720883b297ae44c83375ab053f5f1
Mustafa Ergül <mustafaergul0@gmail.com>
2021-01-06 23:36:18 +0300

    add CC func

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
+  has_one :payment
+  # we will hit multiple tables when users signs up for a account, below;
+  accepts_nested_attributes_for :payment
 end
